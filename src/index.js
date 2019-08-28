ef742ef09d81395f21587ee4f9125addc366d1ca
gianlucamazzeo <gianlucamazzeo@gmail.com>
2019-08-28 13:04:53 +0200

    libraries semantic

--- src/index.js
+++ src/index.js
@@ -10,3 +10,14 @@ const App = () => {
 // Take the react component and show it on the screen
 ReactDOM.render( <App />, document.querySelector('#root') );
 
+// semantic libraries 
+// http://semantic-ui.com
+// https://semantic-ui.com/collections/menu.html
+// https://semantic-ui.com/views/comment.html
+// for installing libraries use search on google
+// semantic ui cdn   ---  https://cdnjs.com/libraries/semantic-ui   
+// cdnjs.cloudflare.com/ajax/libs/semantic.min.js 
+// https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.js
+
+
+
