7715b6a06847df28be4a073e88c92580d8eb0490
gianlucamazzeo <gianlucamazzeo@gmail.com>
2019-08-28 14:49:46 +0200

    add faker and the first component commentDetail

--- src/index.js
+++ src/index.js
@@ -2,25 +2,15 @@
 // https://github.com/StephenGrider/redux-code
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './commentDetail';
 
 const App = () => {
    return (
             <div className="ui container comments">
-                <div className="comment">
-                    <a href="/" className="avatar">
-                        <img alt="avatar" />
-                    </a>
-                        <div className="content">
-                            <a href="/" className="author">
-                                Sam
-                            </a>
-                            <div className="metadata">
-                                <span className="date">Oggi è il 28</span>
-                            </div>
-                            <div className="text">Nice Blog!</div>
-                        </div>
-                    </div>
-                </div>
+                <CommentDetail />
+            </div>
+                
    );
 };
 
