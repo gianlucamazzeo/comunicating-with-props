4370ec326defcbf07fb13d347e6c3ee5ba0b2afd
gianlucamazzeo <gianlucamazzeo@gmail.com>
2019-08-28 12:52:10 +0200

    second commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// Import the React and ReactDOM libraries
+// https://github.com/StephenGrider/redux-code
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+   return <div>Hi there!</div>
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render( <App />, document.querySelector('#root') );
+
